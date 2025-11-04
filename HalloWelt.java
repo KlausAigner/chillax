9a6b5006a86bcec242854298887a141483f7f7a0
Klaus Aigner <kl.aigner@gmail.com>
2025-11-04 11:11:35 +0100

    yaml

---
+++ HalloWelt.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class HalloWelt here.
+ *
+ * @author Klaus Aigner
+ * @version 1.0
+ */
+public class HalloWelt
+{
+    private static IO io = new IO();
+    
+    public static void main (String[] args)
+    {
+        io.println("Hallo Welt");
+    }
+}
